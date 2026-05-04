6f6975a9feef48f8426022a7e7abcd2597a903a2
Bhavya Varshney <btech10566.25@bitmesra.ac.in>
2026-05-04 16:55:44 +0530

    New

--- src/app.js
+++ src/app.js
@@ -5,13 +5,13 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
+    origin: process.env.CORS_ORIGIN,   // used for sending requests from frontend to backend
+    credentials: true                  // used for sending cookies
 }))
-app.use(express.json({ limit: "16kb" }))
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }))  // used for sending JSON data
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))  // used for sending from URL
+app.use(express.static("public"))    // used for serving static files like css, js, images, etc.
+app.use(cookieParser())              // used for sending cookies
 
 
 
