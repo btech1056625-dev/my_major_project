63f2b6967d84af5372a71ddc79f4914589dcb6dd
Bhavya Varshney <btech10566.25@bitmesra.ac.in>
2026-05-22 23:15:39 +0530

    subscription and tweet model bussiness logic

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,10 +1,70 @@
-import {Subscription} from "../models/subscription.model.js" 
-import {asynchandler} from '../utils/asynchandler.js';
+import mongoose, {isValidObjectId} from "mongoose"
+import {User} from "../models/user.model.js"
+import { Subscription } from "../models/subscription.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
 
-const subscribeToChannel = asynchandler(async (req, res) => {
-    return res.status(200).json({
-        message: "Subscribed to channel successfully",
-        success: true
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+    const {channelId} = req.params  // channel id is the id of the user to which current user wants to subscribe or unsubscribe
+    // TODO: toggle subscription
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError("Invalid channel ID", 400)
+    }
+    const channel = await User.findById(channelId)
+    if (!channel) {
+        throw new ApiError("Channel not found", 404)
+    }
+    
+    const isSubscribed = await Subscription.findOne({
+        $and: [
+            {subscriber: req.user._id},
+            {channel: channelId}
+        ]
     })
-})  
-export {subscribeToChannel}
+    if (isSubscribed) {
+        // User is subscribed, so unsubscribe
+        await Subscription.deleteOne({
+            $and: [
+                {subscriber: req.user._id},
+                {channel: channelId}
+            ]
+        })
+        res.status(200).json(new ApiResponse(200, null, "Unsubscribed from channel successfully"))
+    } else {
+        // User is not subscribed, so subscribe
+        const subscription = await Subscription.create({
+            subscriber: req.user._id,
+            channel: channelId
+        })
+        res.status(201).json(new ApiResponse(201, subscription, "Subscribed to channel successfully"))
+    }
+})
+
+// controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const {channelId} = req.params   // channel id is the id of the user whose subscriber list we want to fetch
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError("Invalid channel ID", 400)
+    }
+    const subscribers = await Subscription.find({channel: channelId}).populate("subscriber", "username fullName avatar")
+    res.status(200).json(new ApiResponse(200, subscribers, "Subscriber list fetched successfully"))
+})
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subscriberId } = req.params
+    if (!isValidObjectId(subscriberId)) {
+        throw new ApiError("Invalid subscriber ID", 400)
+    }
+    const channels = await Subscription.find({subscriber: subscriberId}).populate("channel", "username fullName avatar")
+    res.status(200).json(new ApiResponse(200, channels, "Subscribed channels fetched successfully"))
+})
+
+export {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+}
--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,8 +1,58 @@
-import { asynchandler } from "../utils/asynchandler";
-const createTweet = asynchandler(async (req, res) => {
-    return res.status(200).json({
-        message: "Tweet created successfully",
-        success: true
+import mongoose, { isValidObjectId } from "mongoose"
+import {Tweet} from "../models/tweet.model.js"
+import {User} from "../models/user.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const createTweet = asyncHandler(async (req, res) => {
+    //TODO: create tweet
+    const {content} = req.body
+    if (!content || content.trim() === "") {
+        throw new ApiError("Content is required", 400)
+    }   
+    const tweet = await Tweet.create({
+        content,
+        owner: req.user._id // Assuming you have authentication middleware that sets req.user
     })
-})  
-export {createTweet}
+    res.status(201).json(new ApiResponse(201, tweet, "Tweet created successfully"))
+})
+
+const getUserTweets = asyncHandler(async (req, res) => {
+    // TODO: get user tweets
+    const {userId} = req.params
+    if (!isValidObjectId(userId)) {
+        throw new ApiError("Invalid user ID", 400)
+    }
+    const tweets = await Tweet.find({owner: userId})
+    res.status(200).json(new ApiResponse(200, tweets, "Tweets fetched successfully"))
+})
+
+const updateTweet = asyncHandler(async (req, res) => {
+    //TODO: update tweet
+    const {tweetId} = req.params
+    const {content} = req.body
+    if (!content || content.trim() === "") {
+        throw new ApiError("Content is required", 400)
+    }
+    const tweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {content},
+        {new: true}
+    )
+    res.status(200).json(new ApiResponse(200, tweet, "Tweet updated successfully"))
+})
+
+const deleteTweet = asyncHandler(async (req, res) => {
+    //TODO: delete tweet
+    const {tweetId} = req.params
+    const tweet = await Tweet.findByIdAndDelete(tweetId)
+    res.status(200).json(new ApiResponse(200, tweet, "Tweet deleted successfully"))
+})
+
+export {
+    createTweet,
+    getUserTweets,
+    updateTweet,
+    deleteTweet
+}
