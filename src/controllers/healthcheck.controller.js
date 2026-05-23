ce4b5ec73053cf46929f9f60a17b7a581cbb4c8a
Bhavya Varshney <btech10566.25@bitmesra.ac.in>
2026-05-23 18:40:35 +0530

    new controllers bussiness logic

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,15 +1,15 @@
-import { asynchandler } from "../utils/asynchandler";
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
 
-const uploadVideo = asynchandler(async (req, res) => {
-    return res.status(200).json({
-        message: "Video uploaded successfully",
-        success: true
-    })
-})
 
-const getVideo = asynchandler(async (req, res) => {
-    return res.status(200).json({
-        message: "Video fetched successfully",
-        success: true
-    })
-})  
+const healthcheck = asyncHandler(async (req, res) => {
+    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    return res.status(200).json(
+        new ApiResponse("OK", 200, { message: "Server is healthy" })
+    );
+});
+
+export {
+    healthcheck
+    }
